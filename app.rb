3c71164baa96ae3e9ca55f4b675cf3403a837c23
Dan Hintz <dhintz@live.com>
2019-04-15 20:40:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get "/" do
+      erb :"pirates/new"
+    end
+    
+    post "/pirates" do
+      @pirate = Pirate.new(params)
+    end
+    
   end
 end
